dce07cb9bb1bfd9283b86b233708107e30d05606
Lukini <omelyanovskyeb@gmail.com>
2023-12-23 19:27:55 -0300

    fix entrypoint 2

--- .docker/php/docker-entrypoint.sh
+++ .docker/php/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 set -e
 
 composer setup
-php artisan key:generate --force
-php artisan migrate --force
-php artisan db:seed
-php artisan optimize:clear
 
 chmod -R 777 /var/www/storage
 chmod -R 777 /var/www/bootstrap/cache
